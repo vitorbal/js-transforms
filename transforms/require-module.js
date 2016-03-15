f3eaad098f256db79a0a28b34f12286c57ad80cd
Vitor Balocco <vitorbal@gmail.com>
2016-03-15 10:09:31 +0100

    Add better comments for require-module transform

--- transforms/require-module.js
+++ transforms/require-module.js
@@ -1,3 +1,14 @@
+/**
+ * Refactor all require calls to have a different module path prefix
+ * E.g. from:
+ * require('event_promotion/view/calendar_item');
+ * to:
+ * require('bundle/event_promotion/view/calendar_item');
+ *
+ * Initial prefix to look for needs to be passed to jscodeshift as an option
+ * For the example above, the cli call would be like this:
+ * `jscodeshift -t transforms/require-module.js fileA fileB --bundleName=event_promotion`
+ */
 module.exports = function(fileInfo, api, options) {
     'use strict';
 
